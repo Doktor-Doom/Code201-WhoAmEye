6956496017a3e03b98d532ab674229397c644a52
Doktor-Doom <andre.o.martin@gmail.com>
2020-07-10 18:21:08 -0700

    cleaned up a bit more added a prompt at the end

--- js/app.js
+++ js/app.js
@@ -8,19 +8,18 @@
 // - Is Andre tall, i reach all of the shelves
 // - Does Andre like to dance
 
+// === global ===
+var usernaamAntwoord;
 
-//Q1 Origin
 function wieBenJe(){
-  var usernaamAntwoord = prompt('How\'s it going? What\'s your name?');
-  // console.log('usernaamAntwoord :' + usernaamAntwoord);
+  usernaamAntwoord = prompt('How\'s it going? What\'s your name?'); //removed var
+  console.log('usernaamAntwoord :' + usernaamAntwoord);
 
   alert('Cool Beans, ' + usernaamAntwoord + ' hope you\'re having a good day');
 }
 wieBenJe();
 //Q1 Function.. I'd like to add a time of day loop here to tailor the greeting to the time of day..
 
-
-//Q2 Origin
 function waarVanDan(){
   var seattleOrigin = prompt('Am I from around here (Y/N)?');
   //console.log('seattleOrigin:' + seattleOrigin);
@@ -28,13 +27,11 @@ function waarVanDan(){
   if (seattleOrigin.toUpperCase() === 'Y'){
     alert('Not Quite..');
   } else {
-    alert('Appears Legitimate' + usernaamAntwoord);
+    alert('Appears Legitimate ' + usernaamAntwoord);
   } //i need this alert to show when user says 'N' or a loop to try again if wrong
 }
 waarVanDan();
-//Q2 Complete
 
-//Q3 Origin
 function film(){
   var filmLover = prompt('Do I like movies (y/n)?');
   //console.log('filmLover:' + filmLover);
@@ -45,9 +42,7 @@ function film(){
   }
 }
 film();
-//Q3 Complete
 
-//Q4 Origin
 function kin(){
   var famFam = prompt('Does this guy have siblings (y/n) ?');
   // console.log('famFam :' + famFam);
@@ -59,9 +54,7 @@ function kin(){
   }
 }
 kin();
-//Q4 Completed
 
-//Q5 Origin
 function topShelf(){
   var reach = prompt('Is he tall (y/n)?');
   // console.log('reach : ' + reach);
@@ -72,59 +65,55 @@ function topShelf(){
   }
 }
 topShelf();
-//Q5 Complete
 
 function howManyPetsQuestion(){
-  //Question 6 in a loop
-  for(var i=0; i<4; i++){
-    //Question 6 content
+
+  for(var i = 0; i < 4; i ++){
+
     var howManyPetsAnswer = prompt('How many pets does Andre want to have?');
     // console.log('howManyPetsAnswer :' + howManyPetsAnswer);
-    if (howManyPetsAnswer > '30'){
-      alert('That\'s just crazy! Try again.');
-    } else if(howManyPetsAnswer < '30'){
-      alert('Close, but no cigar! Try again.');
+
+    if (howManyPetsAnswer > 30){
+      alert('That\'s just crazy, maybe a bit less! Try again.');
+
+    } else if(howManyPetsAnswer < 30){
+      alert('Close, but no cigar! a few more Try again.');
+
     } else if(howManyPetsAnswer === '30'){
       alert('I know it seems a bit much...and it probably is BUT I don\'t care!');
       break;
     }
     // Thanks to Krystian for helping me understand this
-    // Q6 Content end
   }
-  // Q6 loop complete
 }
 howManyPetsQuestion();
-// Q6 Complete
 
-// Q7 start
-for (var i=0; i<6; i++){
-  // Question 7 array
-  var favColorAnswer = prompt('What is Andre\'s favorite color?');
+function tooManyColors(){ //function declare
+  for (var i=0; i<6; i++){
 
-  var favColorArray = ['purple', 'green', 'neon pink', 'orange', 'navy blue'];
+    var favColorAnswer = prompt('What is Andre\'s favorite color?');
 
-  if(favColorAnswer === favColorArray[0] || favColorAnswer === favColorArray[1] ||favColorAnswer === favColorArray[2]){
-    alert('You got it!');
-    break;
-  } else {
-    alert('A Swing and A Miss.');
-  }
-  // Q7 array end
-}
-//Q7 Loop
+    var favColorArray = ['purple', 'green', 'neon pink', 'orange', 'navy blue'];
 
-
-//Q8 Origin
-
-function danceDance(){
-  for(var i=0; i<=3; i++){
-    var doIDance = prompt('Doe\'s he dance (y/n)?');
-    if (doIDance.toLowerCase() === 'y'){
-      alert ('I only do that to make my wife laugh..');
+    if(favColorAnswer === favColorArray[0] || favColorAnswer === favColorArray[1] ||favColorAnswer === favColorArray[2]){
+      alert('You got it!');
+      break;
     } else {
-      alert('only to make the wife laugh');
+      alert('A Swing and A Miss.');
     }
   }
 }
+tooManyColors(); //function call
+
+function danceDance(){
+  // for(var i=0; i<=3; i++)
+  var doIDance = prompt('Doe\'s he dance (y/n)?');
+  if (doIDance.toLowerCase() === 'y'){
+    alert ('I only do that to make my wife laugh..');
+  } else {
+    alert('only to make the wife laugh');
+  }
+}
 danceDance();
-//Q8 complete
+
+prompt('Thank\'s for stopping by,' + usernaamAntwoord);
